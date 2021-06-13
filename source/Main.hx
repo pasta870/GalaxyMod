85b84d1558175d1efd6adb1ba289816739273229
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-14 00:32:50 +0700

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -14,7 +14,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 120; // How many frames per second the game should run at.
+	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
@@ -66,11 +66,14 @@ class Main extends Sprite
 		#if !debug
 		initialState = TitleState;
 		#end
+		gameWidth = 1280;
+		gameHeight = 720;
+		zoom = 1;
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		//#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
-		#end
+		//#end
 	}
 }
