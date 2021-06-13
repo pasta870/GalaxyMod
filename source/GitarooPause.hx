54d4ffd760552cc784a3a16f57a48bf54bcbe8b2
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-14 00:31:21 +0700

    update GitarooPause.hx

--- source/GitarooPause.hx
+++ source/GitarooPause.hx
@@ -46,7 +46,10 @@ class GitarooPause extends MusicBeatState
 		add(cancelButton);
 
 		changeThing();
-
+//crap but better
+        #if mobileC
+        addVirtualPad(LEFT_RIGHT, A);
+        #end
 		super.create();
 	}
 
